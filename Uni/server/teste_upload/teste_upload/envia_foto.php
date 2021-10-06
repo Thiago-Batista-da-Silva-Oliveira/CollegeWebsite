3035b47dd351ae670c03c6b758e40b339652810d
Thiago <thiagoliveira01@globomail.com>
2021-10-06 19:52:53 -0300

    teste

--- Uni/server/teste_upload/teste_upload/envia_foto.php
+++
@@ -1,9 +0,0 @@
-<html>
-     <head>
-          <title>Envia foto</title> 
-    </head> 
-    <body> 
-        <?php $nome_temporario=$_FILES["Arquivo"]["tmp_name"]; $nome_real=$_FILES["Arquivo"]["name"]; copy($nome_temporario,"imagens/$nome_real"); ?> 
-    </body> 
-</html>
-
